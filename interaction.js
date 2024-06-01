28f18ec61050c531c97598ef7176ecf841e892ca
GesChen <omgtntlol101@gmail.com>
2024-06-01 12:09:29 -0400

    resources not loading fix?

--- interaction.js
+++ interaction.js
@@ -16,3 +16,16 @@ function scrolled() {
         logo.style.setProperty("--logo-hover-size", "83px");
     }
 }
+
+var folder = ".resources/";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $("body").append( "<img src='"+ folder + val +"'>" );
+            } 
+        });
+    }
+});
